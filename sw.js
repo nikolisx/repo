91269eea3fc89b8a57f101ab56f86eff83a35e75
nikolisx <148611410+nikolisx@users.noreply.github.com>
2026-05-23 11:00:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ΚΑΘΕ ΦΟΡΑ ΠΟΥ ΚΑΝΕΙΣ ΑΛΛΑΓΗ ΣΤΟ INDEX.HTML, ΘΑ ΑΛΛΑΖΕΙΣ ΑΥΤΟ ΤΟ ΝΟΥΜΕΡΟ (π.χ. v2, v3, v4)
-const CACHE_NAME = 'emode-calc-v19'; 
+const CACHE_NAME = 'emode-calc-v20'; 
 
 const urlsToCache = [
   './',
